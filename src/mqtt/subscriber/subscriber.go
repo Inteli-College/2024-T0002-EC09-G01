8d86ec5e1395d6b8bc9d780a7088e03a614927db
HallZero <filipikikuchi@gmail.com>
2024-02-14 20:11:42 -0300

    test(subscriber): :white_check_mark: Add tests for subscriber

--- src/mqtt/subscriber/subscriber.go
+++ src/mqtt/subscriber/subscriber.go
@@ -2,26 +2,18 @@ package main
 
 import (
 	"fmt"
-	MQTT "github.com/eclipse/paho.mqtt.golang"
+	DefaultClient "mqtt/src/common"
 )
 
-var messagePubHandler MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
-	fmt.Printf("Received: %s from topic: %s\n", msg.Payload(), msg.Topic())
-}
-
-const broker string = "broker.hivemq.com:1883"
-
 func main() {
-	opts := MQTT.NewClientOptions().AddBroker(broker)
-	opts.SetClientID("go_subscriber")
-	opts.SetDefaultPublishHandler(messagePubHandler)
-
-	client := MQTT.NewClient(opts)
+	
+	client := DefaultClient.CreateClient(DefaultClient.Broker, DefaultClient.IdSubscriber, DefaultClient.Handler)
+	
 	if token := client.Connect(); token.Wait() && token.Error() != nil {
 		panic(token.Error())
 	}
 
-	if token := client.Subscribe("sensors/Sensor1", 1, nil); token.Wait() && token.Error() != nil {
+	if token := client.Subscribe("sensors/SPS30", 1, nil); token.Wait() && token.Error() != nil {
 		fmt.Println(token.Error())
 		return
 	}
--- src/mqtt/subscriber/subscriber_test.go
+++ src/mqtt/subscriber/subscriber_test.go
@@ -1,9 +1,23 @@
 package main
 
-import "testing"
+import (
+	DefaultClient "mqtt/src/common"
+	"testing"
+)
 
-func TestSubscriber(t *testing.T){
-	t.Run("Testing Broker Connection", func(t *testing.T) {
-		return
+func TestSubscriber(t *testing.T) {
+	t.Run("Subscription to topic", func(t *testing.T) {
+		client := DefaultClient.CreateClient(DefaultClient.Broker, DefaultClient.IdSubscriber, DefaultClient.Handler)
+
+		if token := client.Connect(); token.Wait() && token.Error() != nil {
+			t.Error(token.Error())
+		}
+
+		if token := client.Subscribe("sensors/SPS30", 1, nil); token.Wait() && token.Error() != nil {
+			t.Error(token.Error())
+			return
+		}
+
+		t.Log("Subscribed successfully to Topic")
 	})
 }
