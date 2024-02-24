728a57cc94c3e61db0a4669022bca37e95c9e30e
HallZero <filipikikuchi@gmail.com>
2024-02-24 14:18:35 -0300

    test(tests): :white_check_mark: Add files for tests

--- src/tests/client_test.go
+++ src/tests/client_test.go
@@ -1,10 +1,14 @@
 package common
 
-import "testing"
+import (
+	"testing"
+
+	DefaultClient "2024-T0002-EC09-G01/src/pkg/common"
+)
 
 func TestClient(t *testing.T) {
 	t.Run("Create a Client", func(t *testing.T) {
-		client := CreateClient(Broker, "go-mqtt-client", Handler)
+		client := DefaultClient.CreateClient(DefaultClient.Broker, "go-mqtt-client", DefaultClient.Handler)
 
 		if token := client.Connect(); token.Wait() && token.Error() != nil {
 			t.Error(token.Error())
