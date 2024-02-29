82514020da9376b4935ec1eceacaca2c167da7a1
luanaparra <luana.silva@sou.inteli.edu.br>
2024-02-29 08:16:46 -0300

    update client_test.go

--- src/pkg/common/client_test.go
+++ src/pkg/common/client_test.go
@@ -1,6 +1,11 @@
 package common
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
 
 func TestClient(t *testing.T) {
 	t.Run("Create a Client", func(t *testing.T) {
@@ -11,5 +16,72 @@ func TestClient(t *testing.T) {
 		}
 	})
 
-}
+	t.Run("Subscribe to topic", func(t *testing.T) {
+		client := CreateClient(Broker, "go-mqtt-client-subscribe", Handler)
 
+		if token := client.Connect(); token.Wait() && token.Error() != nil {
+			t.Error(token.Error())
+		}
+
+		topic := "test/topic"
+
+		token := client.Subscribe(topic, 1, func(client mqtt.Client, msg mqtt.Message){
+			t.Logf("Received message on topic %s: %s", msg.Topic(), msg.Payload())
+		})
+
+		token.Wait()
+		if token.Error() != nil{
+			t.Error(token.Error())
+		}
+
+		time.Sleep(2 * time.Second)
+
+		token = client.Unsubscribe(topic)
+		token.Wait()
+		if token.Error() != nil {
+			t.Error(token.Error())
+		}
+	})
+
+	t.Run("Publish message", func(t *testing.T) {
+		client := CreateClient(Broker, "go-mqtt-client-publish", Handler)
+
+		if token := client.Connect(); token.Wait() && token.Error() != nil {
+			t.Error(token.Error())
+		}
+
+		topic := "test/topic"
+
+		payload := "Publish"
+
+		token := client.Publish(topic, 1, false, payload)
+
+		token.Wait()
+		if token.Error() != nil{
+			t.Error(token.Error())
+		}
+	})
+
+	t.Run("Authorization test", func(t *testing.T){
+		publishTopic := "test/publish"
+		subscribeTopic := "test/subscribe"
+
+		client := CreateClient(Broker, "go-mqtt-client-auth", Handler)
+
+		if token := client.Connect(); token.Wait() && token.Error() != nil {
+			t.Error(token.Error())
+		}
+
+		token := client.Publish(publishTopic, 1, false, "Authorization Test: Publish")
+		token.Wait()
+		if token.Error() != nil {
+			t.Error(token.Error())
+		}
+
+		token := client.Subscribe(subscribeTopic, 1, nil)
+		token.Wait()
+		if token.Error() != nil {
+			t.Error(token.Error())
+		}
+	})
+}
