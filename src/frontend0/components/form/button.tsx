aac7290e55b77058ba2f4a3d4674dc775b93c1e4
Luca Giberti <luca.giberti@sou.inteli.edu.br>
2024-03-26 07:12:25 -0300

    deleted unecessary folder

--- src/frontend0/components/form/button.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Button: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = ({ children, ...rest }) => {
-  return <button {...rest}>{children}</button>;
-};
-
-export default Button;
