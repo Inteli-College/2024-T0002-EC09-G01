fe4a4a178f137fbe4a736e04d68406fb6451f044
Luca Giberti <luca.giberti@sou.inteli.edu.br>
2024-03-26 06:14:13 -0300

    feat: forms feito falta integração com o back

---
+++ src/frontend0/app.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Form from './components/form/form';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Sensor Input</h1>
+      <Form />
+    </div>
+  );
+};
+
+export default App;
