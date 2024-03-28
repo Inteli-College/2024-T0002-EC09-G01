61150b904162406fdb64a4120252b932fba62e98
igor <igor.garcia@sou.inteli.edu.br>
2024-03-28 07:16:00 -0300

    back&front

--- src/frontend/src/index.tsx
+++ src/frontend/src/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Button from './components/form/button';
+import AlertPage from './alertPage';
+
+const handleSubmit = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <header className="header">
+        <Button onClick={(handleSubmit)}> Voltar </Button>
+      </header>
+      <AlertPage />
+    </React.StrictMode>,
+    document.getElementById('root')  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <header className="header"></header>
+    <header className="header">
+      <Button onClick={(handleSubmit)}> Alertas </Button>
+    </header>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
